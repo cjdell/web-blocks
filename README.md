5ec4cbb2be70791e784d77f3e8bb61c4b26f84e5
Chris Dell <cjdell@gmail.com>
2015-06-02 20:18:15 +0100

    Added another sample

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Web Blocks
 ==========
 
-A WebGL application for building and learning programming. Inspired by Minecraft Pi Edition.
+A WebGL application for building worlds and learning programming. Inspired by Minecraft Pi Edition.
 
 See it in action here: [http://wb.buz.co/]
--- app/Game.js
+++ app/Game.js
@@ -30,7 +30,6 @@ function Game() {
     viewPort = platform.getViewPort();
 
     if (renderer.setClearColor) renderer.setClearColor(0xffffff, 1);
-    //renderer.shadowMapEnabled = true;
 
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 
@@ -76,59 +75,6 @@ function Game() {
         blockMaterial.uniforms.info.value = texture;
       });
 
-      // Tower
-      workerInterface.setBlocks(new THREE.Vector3(5, 0, 5), new THREE.Vector3(10, 6, 10), 2);
-      workerInterface.setBlocks(new THREE.Vector3(5, 7, 5), new THREE.Vector3(10, 10, 10), 1);
-
-      // Sky
-      //workerInterface.setBlocks(new THREE.Vector3(0, 30, 0), new THREE.Vector3(255, 30, 255), 2);
-
-      // Water
-      //workerInterface.setBlocks(new THREE.Vector3(0, 1, 0), new THREE.Vector3(255, 1, 255), 3);
-
-      // Pyramid
-      for (var i = 0; i <= 20; i++) {
-        workerInterface.setBlocks(new THREE.Vector3(30 + i, i, 30 + i), new THREE.Vector3(70 - i, i, 70 - i), (i % 3) + 1);
-      }
-
-      //var c = 0;
-      //
-      //setInterval(function() {
-      //  for (var i = 0; i <= 20; i++) {
-      //    workerInterface.setBlocks(new THREE.Vector3(30 + i, i, 30 + i), new THREE.Vector3(70 - i, i, 70 - i), ((i + c) % 3) + 1, true);
-      //  }
-      //  c++;
-      //}, 2000);
-
-      //var c = 0;
-      //
-      //setInterval(function() {
-      //  workerInterface.setBlocks(new THREE.Vector3(10, 30, 10), new THREE.Vector3(20, 30, 20), (c % 3) + 1, true);
-      //  c++;
-      //}, 1000);
-
-      //setInterval(function() {
-      //  workerInterface.getBlock(new THREE.Vector3(10, 10, 10))
-      //  .then(function(result) {
-      //    console.log(result);
-      //  });
-      //}, 1000);
-
-      for (var xx = 0; xx < 60; xx++) {
-        for (var zz = 0; zz < 60; zz++) {
-          var r = Math.sqrt(Math.pow(xx - 30, 2) + Math.pow(zz - 30, 2));
-          if (r <= 30 && r > 27) {
-            workerInterface.setBlocks(new THREE.Vector3(xx + 20, 0, zz + 20), new THREE.Vector3(xx + 20, 20, zz + 20), 1);
-          }
-          if (r <= 27 && r > 24) {
-            //workerInterface.setBlocks(new THREE.Vector3(xx + 20, 0, zz + 20), new THREE.Vector3(xx + 20, 20, zz + 20), 2);
-          }
-          if (r <= 24 && r > 21) {
-            workerInterface.setBlocks(new THREE.Vector3(xx + 20, 0, zz + 20), new THREE.Vector3(xx + 20, 20, zz + 20), 2);
-          }
-        }
-      }
-
       worldViewer = new WorldViewer(scene, worldInfo, blockMaterial, workerInterface);
 
       var ambientLight = new THREE.AmbientLight(0x777777);
@@ -138,19 +84,6 @@ function Game() {
       var pointLight = new THREE.PointLight(0xffffff, 1.0);
       pointLight.position.set(5.0, 5.0, 5.0);
       scene.add(pointLight);
-
-      //var dirLight = new THREE.DirectionalLight(0xffff00, 1.5);
-      //dirLight.position.set(50.0, 100.0, 50.0);
-      //dirLight.castShadow = true;
-      //dirLight.shadowDarkness = true;
-      //dirLight.shadowCameraVisible = true;
-      //dirLight.shadowCameraRight = 500;
-      //dirLight.shadowCameraLeft = -500;
-      //dirLight.shadowCameraTop = 500;
-      //dirLight.shadowCameraBottom = -500;
-      //dirLight.shadowMapWidth = 4096;
-      //dirLight.shadowMapHeight = 4096;
-      //scene.add(dirLight);
 
       viewPoint = new platform.ViewPoint(camera, pointLight, viewPort, renderer, worldInfo);
 
