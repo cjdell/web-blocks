888106b4e0f5b9f14504544a2f8c95f53a56268c
Chris Dell <cjdell@gmail.com>
2015-06-11 19:14:37 +0100

    Added some missing files

---
+++ app/Api.ts
@@ -0,0 +1,78 @@
+import THREE = require('three');
+
+import wi from './WorkerInterface';
+
+module Api {
+  export interface Api {
+    
+  }
+
+  export function NewApi(workerInterface: wi.WorkerInterface, viewPoint: any): Api {
+    var help = [
+      'Here you can type JavaScript commands!',
+      'Not sure what to type? Here\'s some you can try:',
+      '  setPosition(100,12,110)',
+      '  setBlock(100,10,100,1)',
+      '  setBlocks(100,10,100,100,20,100,1)',
+      'To see more awesome commands, click the "Script" tab and load a sample program! :-)'
+    ].join('\n');
+  
+    var hi = 'Hi there!';
+  
+    var intervalRefs = <number[]>[];
+  
+    function setBlock(x: number, y: number, z: number, type: number, colour: number): void {
+      workerInterface.setBlocks(new THREE.Vector3(x | 0, y | 0, z | 0), new THREE.Vector3(x | 0, y | 0, z | 0), type, colour, true);
+    }
+  
+    function setBlocks(x1: number, y1: number, z1: number, x2: number, y2: number, z2: number, type: number, colour: number): void {
+      workerInterface.setBlocks(new THREE.Vector3(x1 | 0, y1 | 0, z1 | 0), new THREE.Vector3(x2 | 0, y2 | 0, z2 | 0), type, colour, true);
+    }
+  
+    function getBlock(x: number, y: number, z: number): Promise<number> {
+      return workerInterface.getBlock(new THREE.Vector3(x | 0, y | 0, z | 0));
+    }
+  
+    function getPosition(): THREE.Vector3 {
+      return viewPoint.getPosition();
+    }
+  
+    function setPosition(x: number, y: number, z: number): void {
+      viewPoint.setPosition(new THREE.Vector3(x, y, z));
+    }
+  
+    function getTarget(): any {
+      return viewPoint.getTarget();
+    }
+  
+    function setTarget(lon: number, lat: number): void {
+      viewPoint.setTarget({ lon: lon, lat: lat });
+    }
+  
+    function setInterval(func: Function, interval: number): void {
+      var ref = self.setInterval(func, interval);
+      intervalRefs.push(ref);
+    }
+  
+    function clearIntervals(): void {
+      intervalRefs.forEach(self.clearInterval);
+      intervalRefs = [];
+    }
+  
+    return {
+      help: help,
+      hi: hi,
+      setBlock: setBlock,
+      setBlocks: setBlocks,
+      getBlock: getBlock,
+      getPosition: getPosition,
+      setPosition: setPosition,
+      getTarget: getTarget,
+      setTarget: setTarget,
+      setInterval: setInterval,
+      clearIntervals: clearIntervals
+    };
+  }
+}
+
+export default Api;
---
+++ app/WorldViewer.ts
@@ -0,0 +1,164 @@
+import THREE = require('three');
+import _ = require('underscore');
+
+import wi from './WorkerInterface';
+
+module WorldViewer {
+  export interface WorldViewer {
+    exposeNewPartitions(changes:number[]):void
+  }
+  
+  interface PartitionCacheItem {
+    mesh: THREE.Mesh;
+    index: number;
+  }
+  
+  export function NewWorldViewer(scene:THREE.Scene, worldInfo:any, shaderMaterial:THREE.Material, workerInterface:wi.WorkerInterface):WorldViewer {
+    var partitionCaches:PartitionCacheItem[] = null;
+  
+    init();
+  
+    function init() {
+      partitionCaches = new Array(worldInfo.partitionCapacity);
+  
+      addSky();
+    }
+  
+    function getMesh(bufferGeometry:THREE.BufferGeometry, offset:THREE.Vector3):THREE.Mesh {
+      var mesh = new THREE.Mesh(bufferGeometry, shaderMaterial);
+  
+      mesh.position.x += offset.x + 8;
+      mesh.position.y += offset.y + 16;
+      mesh.position.z += offset.z + 8;
+  
+      mesh.castShadow = true;
+      mesh.receiveShadow = true;
+  
+      return mesh;
+    }
+  
+    function addPartition(partitionIndex:number):void {
+      //console.time('addPartition' + partitionIndex);
+  
+      var partitionCache = partitionCaches[partitionIndex];
+  
+      if (!partitionCache) {
+        workerInterface.getPartition(partitionIndex).then(gotPartition);
+        return;
+      }
+  
+      scene.add(partitionCache.mesh);
+    }
+  
+    function updatePartition(partitionIndex:number) {
+      //console.time('updatePartition' + partitionIndex);
+  
+      workerInterface.getPartition(partitionIndex).then(gotPartition);
+    }
+  
+    workerInterface.addChangeListener(function(data:any) {
+      var changeIndices = <number[]>data.changes;
+      var visibleIndices = <number[]>getVisiblePartitionIndices();
+  
+      //console.log('changeIndices', changeIndices);
+      //console.log('visibleIndices', visibleIndices);
+  
+      var toUpdate = _.intersection(changeIndices, visibleIndices);
+  
+      //console.log('toUpdate', toUpdate);
+  
+      toUpdate.forEach(function(index) {
+        updatePartition(index);
+      });
+    });
+  
+    function getVisiblePartitionIndices() {
+      return partitionCaches
+      .filter(function(partitionCache) {
+        return partitionCache.mesh !== null;
+      })
+      .map(function(partitionCache) {
+        return partitionCache.index;
+      });
+    }
+  
+    function gotPartition(data:any) {
+      var geo = data.geo, partitionIndex = data.index;
+  
+      var partitionCache = partitionCaches[partitionIndex];
+  
+      if (partitionCache) {
+        scene.remove(partitionCache.mesh);
+  
+        partitionCache.mesh = null;
+      }
+  
+      var bufferGeometry = new THREE.BufferGeometry();
+  
+      bufferGeometry.addAttribute('position', new THREE.BufferAttribute(geo.data.position, 3));
+      bufferGeometry.addAttribute('normal', new THREE.BufferAttribute(geo.data.normal, 3));
+      bufferGeometry.addAttribute('uv', new THREE.BufferAttribute(geo.data.uv, 2));
+      bufferGeometry.addAttribute('data', new THREE.BufferAttribute(geo.data.data, 4));
+      bufferGeometry.addAttribute('offset', new THREE.BufferAttribute(geo.data.offset, 1));
+  
+      bufferGeometry.computeBoundingSphere();
+      bufferGeometry.computeBoundingBox();
+  
+      var mesh = getMesh(bufferGeometry, geo.offset);
+  
+      partitionCache = {
+        index: partitionIndex,
+        mesh: mesh
+      };
+  
+      partitionCaches[partitionIndex] = partitionCache;
+  
+      scene.add(partitionCache.mesh);
+  
+      //console.timeEnd('addPartition' + partitionIndex);
+      //console.timeEnd('updatePartition' + partitionIndex);
+    }
+  
+    function removePartition(partitionIndex:number) {
+      var partitionCache = partitionCaches[partitionIndex];
+  
+      if (!partitionCache) return;
+  
+      scene.remove(partitionCache.mesh);
+    }
+  
+    function exposeNewPartitions(changes:any) {
+      changes.toBeAdded.forEach(function(partitionIndex:number) {
+        //console.log('toBeAdded', partitionIndex);
+        addPartition(partitionIndex);
+      });
+  
+      changes.toBeRemoved.forEach(function(partitionIndex:number) {
+        removePartition(partitionIndex);
+      });
+    }
+  
+    function addSky() {
+      var geometry = new THREE.PlaneBufferGeometry(1, 1);
+      var material = new THREE.MeshBasicMaterial({ color: 0xbbccff, side: THREE.DoubleSide });
+      var plane = new THREE.Mesh(geometry, material);
+  
+      plane.position.x = worldInfo.blockDimensions.x / 2;
+      plane.position.y = 100;
+      plane.position.z = worldInfo.blockDimensions.z / 2;
+  
+      plane.rotation.x = Math.PI / 2;
+  
+      plane.scale.x = (worldInfo.blockDimensions.x * 100);
+      plane.scale.y = (worldInfo.blockDimensions.z * 100);
+  
+      scene.add(plane);
+    }
+  
+    return {
+      exposeNewPartitions: exposeNewPartitions
+    };
+  }
+}
+
+export default WorldViewer;
