2d4ef14e5d554a573e3bf52de7523261c32dc1f3
Jason McGhee <jmcghee@pandora.com>
2016-06-04 13:16:27 -0700

    an attempt to make the scripts more user friendly

--- samples/BoundPillar.js
+++ samples/BoundPillar.js
@@ -1,4 +1,4 @@
-// Press "6" to make a pillar 10 blocks away!
+// Press "6" to make a pillar 10 blocks away, 5 blocks high!
 bindScript(function () {
     var p = getPosition();
     setBlocks(p.x, p.y, p.z - 10, p.x, p.y + 5, p.z - 10, BlockType.Stone, 1);
--- samples/Cube.js
+++ samples/Cube.js
@@ -1,1 +1,3 @@
-setBlocks(95,5,95,105,15,105,BlockType.Stone);
+// Make a 10 x 10 x 10 cube originating at 'pos'
+var pos = {x: 95, y: 5, z: 95};
+setBlocks(pos.x, pos.y, pos.z, pos.x + 10, pos.y + 10, pos.z + 10, BlockType.Stone);
